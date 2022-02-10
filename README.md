58a369ca6094de0b341eea61e83e50c010a07478
AamirKhattak <aamir.fui@gmail.com>
2022-02-10 11:19:57 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-6ddfrd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-6ddfrd)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+let amount:any = 25;
+amount = 'asd'
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
